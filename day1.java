8d9b306257a71e4e7ecba3f51e237cfbb7885b62
tanya0404 <149752404+tanya0404@users.noreply.github.com>
2025-03-03 23:48:30 +0530

    Day 1

---
+++ day1.java
@@ -0,0 +1,56 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class day1 {
+    
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int[] merg=new int[nums1.length + nums2.length];
+        for(int i=0;i<nums1.length;i++){
+            merg[i]=nums1[i];
+        }
+        for(int i=0;i<nums2.length;i++){
+            merg[nums1.length+i]=nums2[i];
+        }
+        Arrays.sort(merg);
+        int n = merg.length;
+        if (n % 2 == 1) {
+            return merg[n / 2]; 
+        } else {
+            return (merg[n / 2 - 1] + merg[n / 2]) / 2.0; 
+        }
+}
+
+
+
+
+
+
+
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+      for(int i=0;i<nums.length;i++){
+        for(int j=i+1;j<nums.length;j++){
+            if(nums[i]+nums[j]==target){
+                return new int[]{i,j};
+            }
+        }
+      }
+        return new int[]{};
+
+    }
+}
+
+}
+
+
+echo "# leetcode" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/tanya0404/leetcode.git
+git push -u origin main
+
+
+
